b51093a0fb434a140efe4ff86fb64250ebfc4126
akashdeeptechalphonic <akashdeep@techalphonic.com>
2026-01-09 11:47:02 +0530

    make changes according to client

--- src/Components/UserAccount/Addresses/AddressModal.jsx
+++ src/Components/UserAccount/Addresses/AddressModal.jsx
@@ -310,7 +310,7 @@ const AddressModal = ({
                   <input
                     type="text"
                     className="form-control"
-                    value={landmark}
+                    value={landmark} required
                     onChange={(e) => setLandmark(e.target.value)}
                   />
                 </div>
